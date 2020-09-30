c692aacc4cdc742434afd11ac2ecb093c42a599a
Wellington Lourenço Borges <53665329+wlborges@users.noreply.github.com>
2020-09-29 22:09:08 -0300

    v2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles.css'
 
-import logo from '../../assets/logo.png';
+import img from '../../assets/logo.png';
 export default function Home(){
     return(
         <div className="container">
-            <img className="logo"src={logo} alt="logo-construnana"/>
-            <h1 className="title">Site em manutenção.</h1>
+            <div className="logo">
+                <img className="imglogo" src={img} alt="logo"/>
+                <div className="title">Site em Manutenção</div>
+            </div>
         </div>
 
     );
