3ec5f5d3e69c854658bfcee35602c6f0dd88b21a
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2020-09-29 21:31:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jackpot
+Game jackpot
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../src/global.css';
+import Home from '../src/pages/Home';
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
